94d443af02be745fb1f5563576bd29a8f7f59e00
jjsuperpower <jjs29356@gmail.com>
2025-12-04 23:09:38 -0500

    run nix fmt

--- 1-hour_coding/cgol_bevy/nix/modules/devshell.nix
+++ 1-hour_coding/cgol_bevy/nix/modules/devshell.nix
@@ -1,49 +1,62 @@
 { inputs, ... }:
 {
-  perSystem = { config, self', pkgs, lib, ... }: {
-    devShells.default = pkgs.mkShell {
-      name = "cgof_bevy-shell";
-      inputsFrom = [
-        self'.devShells.rust
-        config.pre-commit.devShell # See ./nix/modules/pre-commit.nix
-      ];
-      buildInputs = with pkgs; [
-        pkg-config
-      ]
-      # https://github.com/bevyengine/bevy/blob/latest/docs/linux_dependencies.md
-      ++ lib.optionals (lib.strings.hasInfix "linux" stdenv.hostPlatform.system) [
-        # for Linux
-        # Audio (Linux only)
-        alsa-lib
-        # Cross Platform 3D Graphics API
-        vulkan-loader
-        # For debugging around vulkan
-        vulkan-tools
-        # Wayland support
-        wayland
-        wayland-protocols
-        # Other dependencies
-        libudev-zero
-        xorg.libX11
-        xorg.libXcursor
-        xorg.libXi
-        xorg.libXrandr
-        libxkbcommon
-      ];
-      packages = with pkgs; [
-        fish
-        just
-        nixd # Nix language server
-        bacon
-      ];
-      LD_LIBRARY_PATH = lib.makeLibraryPath (with pkgs; [
-        vulkan-loader
-        xorg.libX11
-        xorg.libXi
-        xorg.libXcursor
-        libxkbcommon
-        wayland
-      ]);
+  perSystem =
+    {
+      config,
+      self',
+      pkgs,
+      lib,
+      ...
+    }:
+    {
+      devShells.default = pkgs.mkShell {
+        name = "cgof_bevy-shell";
+        inputsFrom = [
+          self'.devShells.rust
+          config.pre-commit.devShell # See ./nix/modules/pre-commit.nix
+        ];
+        buildInputs =
+          with pkgs;
+          [
+            pkg-config
+          ]
+          # https://github.com/bevyengine/bevy/blob/latest/docs/linux_dependencies.md
+          ++ lib.optionals (lib.strings.hasInfix "linux" stdenv.hostPlatform.system) [
+            # for Linux
+            # Audio (Linux only)
+            alsa-lib
+            # Cross Platform 3D Graphics API
+            vulkan-loader
+            # For debugging around vulkan
+            vulkan-tools
+            # Wayland support
+            wayland
+            wayland-protocols
+            # Other dependencies
+            libudev-zero
+            xorg.libX11
+            xorg.libXcursor
+            xorg.libXi
+            xorg.libXrandr
+            libxkbcommon
+          ];
+        packages = with pkgs; [
+          fish
+          just
+          nixd # Nix language server
+          bacon
+        ];
+        LD_LIBRARY_PATH = lib.makeLibraryPath (
+          with pkgs;
+          [
+            vulkan-loader
+            xorg.libX11
+            xorg.libXi
+            xorg.libXcursor
+            libxkbcommon
+            wayland
+          ]
+        );
 
         RUST_LOG = "info";
 
